90e3b5a7bf9bdbbdc0da316db99a760a08c850f5
Estefany <estefany.espinoza.h@gmail.com>
2021-12-07 17:17:41 -0300

    trabajo ok

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,122 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="titulo">Random Gif Cat</h1>
+
+    <form class="form_gif">
+      <label for="" class="margin">Titulo: </label>
+      <input type="text" v-model="tituloGif" class="margin"><br>
+      <!--seleccion del filtro -->
+      <label for="" class="margin">Filtro: </label>
+      <select @change.prevent="changeFilter " class="margin">
+        <option v-for="filtro in filtroOpc" :key="filtro.value" :value="filtro.value">{{filtro.filter}}</option>
+      </select><br>
+
+      <!--seleccion del color -->
+      <label for="" class="margin">Color: </label>
+      <select @change.prevent="changeColor" class="margin">
+        <option v-for="color in colorOpc" :key="color.value" :value="color.value">{{color.color}}</option>
+      </select><br>
+
+      <!--seleccion del tamaño-->
+      <label for="" class="margin">Tamaño: </label>
+      <input type="text" v-model="size" class="margin">
+    </form>
+    
+
+    <button @click.prevent="fetchCats" class="margin">Obtener mi gatito</button><br>
+
+    <img v-if="image != `  `" :src="image" alt="gatito" class="margin">
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      tituloGif: " ",
+      image: `  `,
+      size: " ",
+      filter: "blur",
+      color: "green",
+      filtroOpc:[
+        {
+          filter:"Blur",
+          value:"blur",
+        },
+        {
+          filter:"Mono",
+          value: "mono",
+        },
+        {
+          filter:"Sepia",
+          value: "sepia",
+        },
+        {
+          filter:"Negative",
+          value: "negative",
+        },
+        {
+          filter:"Paint",
+          value: "paint",
+        },
+        {
+          filter:"Pixel",
+          value: "pixel",
+        },
+
+      ],
+      colorOpc:[
+        {
+          color:"Green",
+          value:"green",
+        },
+        {
+          color:"Red",
+          value:"red",
+        },
+        {
+          color:"Blue",
+          value:"blue",
+        },
+        {
+          color:"Yellow",
+          value:"yellow",
+        },
+        {
+          color:"Purple",
+          value:"purple",
+        },
+        {
+          color:"Orange",
+          value:"orange",
+        },
+      ]
+    }
+  },
+  computed:{ 
+  },
+  methods:{
+    fetchCats(){
+    this.image =`https://cataas.com/cat/gif/says/${this.tituloGif}?filter=${this.filter}&color=${this.color}&size=${this.size}&type=or `
+    },
+    changeFilter (event){
+      //this.filtroOpc = event.target.value
+      this.filter = event.target.value
+    },
+
+    changeColor (event){
+      //this.colorOpc = event.target.value
+      this.color = event.target.value
+    },
+
+
+  },
+
   components: {
-    HelloWorld
+    
   }
 }
 </script>
@@ -22,7 +127,27 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
+  align-content: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: #9fc7df;
+ 
 }
+
+.titulo{
+  padding-top: 60px ;
+  margin-bottom: 50px;
+}
+
+.form_gif{
+  background-color: #ff637D;
+  padding-top: 50px;
+  padding-bottom: 50px;
+  
+}
+
+.margin{
+  margin: 20px 0 20px 0;
+}
+
+
 </style>
